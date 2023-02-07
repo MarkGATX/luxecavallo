71bb986e26f510aad4789bdec912cbe2117b1ff6
Mark Gardner <themarkgardner@gmail.com>
2023-02-07 15:02:34 -0600

    added custom light theme to ThemeProvider. Changed sx styling to reference new theming

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import Header from './components/Header';
+import { createTheme } from '@mui/material';
+import {ThemeProvider} from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
+
+let cavaleLightTheme = createTheme({
+  palette: {
+    mode:'light',
+    background: {
+      default: '#fffbff'
+    },
+    
+    primary: {
+      main: '#ffe170',
+      contrastText: '#221b00'
+    },
+    secondary: {
+      main: '#705d00',
+      contrastText: '#ffffff'
+    },
+    tertiary: {
+      main:'#4d616c',
+      contrastText:'#ffffff',
+    },
+    headerBack: {
+      main:'#1d1b16',
+      contrastText:"#ffe170",
+    },
+    text : {
+      main:"#1d1b16",
+    },
+    contrastThreshold: 4.5,
+    tonalOffset: .2,
+  },
+  typography: {
+    h1: {
+      fontFamily: 'aviano-flare'
+    },
+    h2: {
+      fontFamily: 'aviano-flare'
+    },
+    h3: {
+      fontFamily: 'aviano-flare'
+    },
+    h4: {
+      fontFamily: 'aviano-flare'
+    },
+    h5: {
+      fontFamily: 'aviano-flare'
+    },
+    h6: {
+      fontFamily: 'aviano-flare'
+    }
+  },
+})
+
 
 export default function App() {
   return (
-    <Header />
-   
+    <ThemeProvider theme={cavaleLightTheme}>
+      <CssBaseline/>
+        <Header />
+   </ThemeProvider>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,21 +3,23 @@ import { Typography } from "@mui/material";
 import cavaleLogo from '../../images/cavale_logo.svg'
 import './Header.css'
 
+const sideWidth = 200;
+
 export default function Header() {
     return (
         <>
         
-        <Box sx={{width:"300px", minHeight:'100vh', backgroundColor:'#1d1b16', position:'absolute', overflow:'hidden'}}>
-            <Box sx={{width:'300px', display: 'flex',justifyContent: 'flex-end'}}>
+        <Box sx={{width:sideWidth, minHeight:'100vh', backgroundColor:'headerBack.main', position:'absolute', overflow:'hidden'}}>
+            <Box sx={{width:'200px', display: 'flex',justifyContent: 'flex-end', paddingTop:'1em'}}>
             <img src={cavaleLogo}  className='cavaleLogoGold'></img>
             </Box>
-            <Typography variant='h2' component='h1' sx={{color:'#ffe170', writingMode:'vertical-lr', letterSpacing:'-.6em',  textOrientation:'upright', position:'relative',left:'255px'}}>LUXE CAVALE</Typography>
+            <Typography variant='h2' component='h1' sx={{color:'headerBack.contrastText', writingMode:'vertical-lr', letterSpacing:'-.6em',  textOrientation:'upright', position:'relative',left:'155px'}}>LUXE CAVALE</Typography>
         </Box>
-        <Box sx={{position:'absolute', minHeight:'100vh', left:'300px', overflow:'hidden'}}>
-            <Box sx={{width:'300px'}}>
+        <Box sx={{position:'absolute', minHeight:'100vh', left:sideWidth, overflow:'hidden'}}>
+            <Box sx={{width:sideWidth,  paddingTop:'1em'}}>
             <img src={cavaleLogo} className='cavaleLogo'></img> 
             </Box>
-            <Typography variant='h2' component='h1' sx={{color:'#1d1b16', writingMode:'vertical-lr', letterSpacing:'-.6em',   textOrientation:'upright', position:'relative',left:'-45px'}}>LUXE CAVALE</Typography>
+            <Typography variant='h2' component='h1' sx={{color:'text.main', writingMode:'vertical-lr', letterSpacing:'-.6em',   textOrientation:'upright', position:'relative',left:'-45px'}}>LUXE CAVALE</Typography>
         </Box>
         </>
     )
