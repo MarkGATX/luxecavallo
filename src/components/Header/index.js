733ae591222e2175beb5b42320b1b50626a97233
Mark Gardner <themarkgardner@gmail.com>
2023-02-07 12:55:41 -0600

    added cavale logo and positioned with calc in css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,23 @@
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
+import cavaleLogo from '../../images/cavale_logo.svg'
+import './Header.css'
 
 export default function Header() {
     return (
         <>
-        <Box sx={{width:"300px", minHeight:'100vh', backgroundColor:'#000000', position:'absolute', overflow:'hidden'}}>
-            <Typography variant='h2' component='h1' sx={{color:'#ffffff', writingMode:'vertical-lr', letterSpacing:'-.3em',  textOrientation:'upright', position:'relative',left:'255px'}}>LUXE CAVALE</Typography>
+        
+        <Box sx={{width:"300px", minHeight:'100vh', backgroundColor:'#1d1b16', position:'absolute', overflow:'hidden'}}>
+            <Box sx={{width:'300px', display: 'flex',justifyContent: 'flex-end'}}>
+            <img src={cavaleLogo}  className='cavaleLogoGold'></img>
+            </Box>
+            <Typography variant='h2' component='h1' sx={{color:'#ffe170', writingMode:'vertical-lr', letterSpacing:'-.6em',  textOrientation:'upright', position:'relative',left:'255px'}}>LUXE CAVALE</Typography>
         </Box>
         <Box sx={{position:'absolute', minHeight:'100vh', left:'300px', overflow:'hidden'}}>
-            <Typography variant='h2' component='h1' sx={{color:'#000000', writingMode:'vertical-lr', letterSpacing:'-.3em',   textOrientation:'upright', position:'relative',left:'-45px'}}>LUXE CAVALE</Typography>
+            <Box sx={{width:'300px'}}>
+            <img src={cavaleLogo} className='cavaleLogo'></img> 
+            </Box>
+            <Typography variant='h2' component='h1' sx={{color:'#1d1b16', writingMode:'vertical-lr', letterSpacing:'-.6em',   textOrientation:'upright', position:'relative',left:'-45px'}}>LUXE CAVALE</Typography>
         </Box>
         </>
     )
