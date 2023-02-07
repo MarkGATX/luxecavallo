a295fa625475cb589a9ef2b24f16b1b8f110d1cc
Mark Gardner <themarkgardner@gmail.com>
2023-02-07 16:04:28 -0600

    changed some styling in index header to prep for adding menu drawer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import cavaleLogo from '../../images/cavale_logo.svg'
+import MenuIcon from '@mui/icons-material/Menu';
 import './Header.css'
 
 const sideWidth = 200;
@@ -10,16 +11,18 @@ export default function Header() {
         <>
         
         <Box sx={{width:sideWidth, minHeight:'100vh', backgroundColor:'headerBack.main', position:'absolute', overflow:'hidden'}}>
-            <Box sx={{width:'200px', display: 'flex',justifyContent: 'flex-end', paddingTop:'1em'}}>
-            <img src={cavaleLogo}  className='cavaleLogoGold'></img>
+            
+            <Box sx={{width:'200px', display: 'flex',justifyContent: 'flex-end',alignItems:'center', paddingTop:'1em'}}>
+            <MenuIcon className='menuIcon' sx={{color:'headerBack.contrastText'}}/>
+            <img src={cavaleLogo} alt='Gold Luxe Cavale Logo' className='cavaleLogoGold'></img>
             </Box>
             <Typography variant='h2' component='h1' sx={{color:'headerBack.contrastText', writingMode:'vertical-lr', letterSpacing:'-.6em',  textOrientation:'upright', position:'relative',left:'155px'}}>LUXE CAVALE</Typography>
         </Box>
         <Box sx={{position:'absolute', minHeight:'100vh', left:sideWidth, overflow:'hidden'}}>
             <Box sx={{width:sideWidth,  paddingTop:'1em'}}>
-            <img src={cavaleLogo} className='cavaleLogo'></img> 
+            <img src={cavaleLogo} alt='Dark Luxe Cavale Logo' className='cavaleLogo'></img> 
             </Box>
-            <Typography variant='h2' component='h1' sx={{color:'text.main', writingMode:'vertical-lr', letterSpacing:'-.6em',   textOrientation:'upright', position:'relative',left:'-45px'}}>LUXE CAVALE</Typography>
+            <Typography variant='h2' component='h1' sx={{color:'text.main', writingMode:'vertical-lr', letterSpacing:'-.6em',   textOrientation:'upright', position:'relative',left:'-45px', fontWeight:'900'}}>LUXE CAVALE</Typography>
         </Box>
         </>
     )
