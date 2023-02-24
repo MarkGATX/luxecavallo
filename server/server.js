90f6eb6cd492750ea18b49cc14b738ca900c2413
Mark Gardner <themarkgardner@gmail.com>
2023-02-24 15:34:21 -0600

    changed uri to remove +srv

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
 dotenv.config()
-console.log(process.env +' environment')
+console.log(process.env.MONGODB_URI +' environment')
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
