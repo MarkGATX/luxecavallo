bd573d5264d09b1b6e1ac34419f23747fc700e70
Mark Gardner <themarkgardner@gmail.com>
2023-02-23 21:38:28 -0600

    added video

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Typography } from "@mui/material"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-// import splashVid from '../../images/cavallo_bg_vid4.mp4'
+import splashVid from '../../images/cavallo_bg_vid5.mp4'
 import './Home.css'
 
 export default function Home() {
     return (
         <>
         <Grid2 container component="main"  sx={{width: `calc(100% - 200px)`, left:'200px', zIndex:'-1', overflow:'hidden'}}>
-               {/* <video autoPlay muted loop id="splashVid" >
+               <video autoPlay muted loop id="splashVid" >
                     <source src={splashVid} type="video/mp4" />
-                </video> */}
+                </video>
         </Grid2>
         </>
     )
