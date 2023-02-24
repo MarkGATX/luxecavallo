9c46acc770f4021d0d85c28fc0213231f2c3b371
Mark Gardner <themarkgardner@gmail.com>
2023-02-24 00:25:20 -0600

    added words and animations to the home page to go with video

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -25,15 +25,16 @@ import { useMenuContext } from "../../utils/menuContext";
 import { Link } from "react-router-dom";
 import { memo } from "react";
 import StraightenIcon from '@mui/icons-material/Straighten';
+import HomeIcon from '@mui/icons-material/Home';
 
 
 const sideWidth = 200;
 let drawerWidth = 200;
-const menuItems = [{ 'Men': MaleIcon }, { 'Women': FemaleIcon }, { 'Accessories': DiamondIcon }, { 'Houseware': BlenderIcon }]
-const contactItems = [{'Sizing': StraightenIcon}, { 'About': InfoIcon }, { 'Contact': CallIcon } ]
+const menuItems = [{ 'Home': HomeIcon }, { 'Men': MaleIcon }, { 'Women': FemaleIcon }, { 'Accessories': DiamondIcon }, { 'Houseware': BlenderIcon }]
+const contactItems = [{ 'Sizing': StraightenIcon }, { 'About': InfoIcon }, { 'Contact': CallIcon }]
 
 
-const Header=() =>{
+const Header = () => {
     // const [currentDrawer, setCurrentDrawer] = useState(false);
     const [menuAction, setMenuAction] = useState('Open')
     const { isMenuOpen, toggleMenu } = useMenuContext();
@@ -49,7 +50,7 @@ const Header=() =>{
         console.log(isMenuOpen)
         const menuDrawer = document.querySelector('.MuiDrawer-paper')
         // true = drawer open, so if false, open drawer
-        {isMenuOpen ? setMenuAction('Open') : setMenuAction('Close')}
+        { isMenuOpen ? setMenuAction('Open') : setMenuAction('Close') }
         if (!isMenuOpen) {
             gsap.to(sideContrastText.current, { duration: 1, x: -120 })
             gsap.to(sideContrastHorse.current, { duration: 1, x: -120 })
@@ -74,9 +75,9 @@ const Header=() =>{
             <Box sx={{ width: sideWidth, minHeight: '100vh', backgroundColor: 'headerBack.main', position: 'absolute', overflow: 'hidden' }}>
 
                 <Box sx={{ width: '200px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', paddingTop: '1em' }}>
-                    <Box ref={burger} sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
-                        <MenuIcon  className='menuIcon' onClick={() => slideDrawer()} sx={{ color: 'headerBack.contrastText' }} />
-                        <Typography variant='subtitle2' sx={{color:'headerBack.contrastText'}}>{menuAction}</Typography>
+                    <Box ref={burger} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                        <MenuIcon className='menuIcon' onClick={() => slideDrawer()} sx={{ color: 'headerBack.contrastText' }} />
+                        <Typography variant='subtitle2' sx={{ color: 'headerBack.contrastText' }}>{menuAction}</Typography>
                     </Box>
                     <img src={cavaleLogo} ref={sideContrastHorse} alt='Gold Luxe Cavale Logo' className='cavaleLogoGold'></img>
                 </Box>
@@ -110,17 +111,29 @@ const Header=() =>{
                             const Icon = Object.values(item)[0];
                             console.log(Icon)
                             return (
-                                <Link key={index} to={`/${text}`}>
-                                    <ListItem disablePadding>
-                                        {/* <ListItemButton onClick={() => handlePageChange(text)}> */}
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <Icon />
-                                            </ListItemIcon>
-                                            <ListItemText primary={text} />
-                                        </ListItemButton>
-                                    </ListItem>
-                                </Link>
+                                text === 'Home' ? (<Link key={index} to={`/`}>
+                                <ListItem disablePadding>
+
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <Icon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                                </Link> ) : (
+                                    <Link key={index} to={`/${text}`}>
+                                        <ListItem disablePadding>
+
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <Icon />
+                                                </ListItemIcon>
+                                                <ListItemText primary={text} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>)
+                            
                             );
                         })}
                     </List>
@@ -146,7 +159,7 @@ const Header=() =>{
                     </List>
                 </Drawer>
                 <Box sx={{ width: sideWidth, paddingTop: '1em' }}>
-                    <img src={cavaleLogo} ref={mainContrastHorse} alt='Dark Luxe Cavale Logo' className='cavaleLogo'></img>
+                    <Link to={`/`}><img src={cavaleLogo} ref={mainContrastHorse} alt='Dark Luxe Cavale Logo' className='cavaleLogo'></img></Link>
                 </Box>
                 <Typography className='headerTitle' ref={mainContrastText} variant='h2' component='h1' sx={{ color: 'text.main', writingMode: 'vertical-lr', letterSpacing: '-.6em', textOrientation: 'upright', position: 'relative', left: '-45px', fontWeight: '900' }}>LUXE CAVALLO</Typography>
             </Box>
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,17 +1,83 @@
 import React from "react";
 import { Typography } from "@mui/material"
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import splashVid from '../../images/cavallo_bg_vid5.mp4'
 import './Home.css'
+import gsap from "gsap";
+import Box from "@mui/material/Box";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+
+
 
 export default function Home() {
+    const elegant = useRef();
+    const exquisite = useRef();
+    const luxe = useRef();
+    const fashion = useRef();
+    const accessory = useRef();
+    const more = useRef();
+    const curated = useRef();
+    
+    
+
+
+    useEffect(() => {
+        const video = document.querySelector('video');
+  const tl = gsap.timeline({repeat:-1});
+
+
+        tl.fromTo(elegant.current, { opacity: 0 }, { opacity: .7, duration: 1 }, '+=1.2');
+        tl.to(elegant.current, { opacity: 0, duration: 1 }, '+=5.5');
+        tl.fromTo(exquisite.current, { opacity: 0 }, { opacity: .7, duration: 1 }, '-=.5');
+        tl.to(exquisite.current, { opacity: 0, duration: 1 }, '+=3.7');
+        tl.fromTo(luxe.current, { opacity: 0 }, { opacity: .7, duration: 1 }, '-=.5');
+        tl.fromTo(fashion.current, { opacity: 0 }, { opacity: .7, duration: 1 });
+        tl.to(fashion.current, { opacity: 0, duration: 1 }, '+=2');
+        tl.fromTo(accessory.current, { opacity: 0 }, { opacity: .7, duration: 1 }, '-=.5');
+        tl.to(accessory.current, { opacity: 0, duration: 1 }, '+=2');
+        tl.fromTo(more.current, { opacity: 0 }, { opacity: .7, duration: 1 }, '-=.5');
+        tl.to(more.current, { opacity: 0, duration: 1 }, '+=2');
+        tl.fromTo(curated.current, { opacity: 0 }, { opacity: .7, duration: 1 });
+        tl.to(curated.current, { opacity: 0, duration: 1 }, '+=12');
+        tl.to(luxe.current, { opacity: 0, duration: 1 }, '-=1');
+        tl.play();
+
+        
+    },[])
+
+
+
+
     return (
         <>
-        <Grid2 container component="main"  sx={{width: `calc(100% - 200px)`, left:'200px', zIndex:'-1', overflow:'hidden'}}>
-               <video autoPlay muted loop id="splashVid" >
+            <Box component="main" sx={{ width: `calc(100% - 200px)`, left: '200px', zIndex: '-1', overflow: 'hidden' }}>
+                <video autoPlay muted loop id="splashVid" >
                     <source src={splashVid} type="video/mp4" />
                 </video>
-        </Grid2>
+                <Box ref={elegant} sx={{ width: '100%', position: 'absolute', top: '170px', left: '230px' }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '3vw !important', color: 'primary.main', fontWeight: '500', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>Elegant</Typography>
+                </Box>
+                <Box ref={exquisite} sx={{ width: '100%', position: 'absolute' }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '3vw !important', position: 'absolute', top: '470px', right: '150px', color: 'primary.main', fontWeight: '500', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>Exquisite</Typography>
+                </Box>
+                <Box ref={luxe} sx={{ width: '100%', position: 'absolute', top: '55%', }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '5.5vw !important', textAlign: 'center', fontWeight: '500', color: 'primary.main', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>Luxe Cavallo</Typography>
+                </Box>
+                <Box ref={fashion} sx={{ width: '100%', position: 'absolute', top: '70%', }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '3vw !important', textAlign: 'center', color: 'primary.main', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>Fine fashion,</Typography>
+                </Box>
+                <Box ref={accessory} sx={{ width: '100%', position: 'absolute', top: '70%', }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '3vw !important', textAlign: 'center', color: 'primary.main', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>accessories,</Typography>
+                </Box>
+                <Box ref={more} sx={{ width: '100%', position: 'absolute', top: '70%', }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '3vw !important', textAlign: 'center', color: 'primary.main', WebkitTextStroke: `2px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>and more.</Typography>
+                </Box>
+                <Box ref={curated} sx={{ width: '100%', position: 'absolute', top: '70.5%', }}>
+                    <Typography variant='h1' className="videoText" sx={{ fontSize: '2.vw !important', textAlign: 'center', color: 'primary.main', WebkitTextStroke: `1px`, WebkitTextStrokeColor: (theme) => theme.palette.headerBack.main }}>Curated collections for discerning clients.</Typography>
+                </Box>
+
+            </Box>
         </>
     )
 }
