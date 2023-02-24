80391bb0735bdd0263ccd40371ae56dee79fa88a
Mark Gardner <themarkgardner@gmail.com>
2023-02-24 15:57:07 -0600

    typo in mongo uri

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import * as dotenv from 'dotenv'
-dotenv.config()
+
 
 
 const { typeDefs, resolvers } = require('./schemas');
