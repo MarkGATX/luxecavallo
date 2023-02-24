30b0531f5bdea0a6b76fa43c8eacab26965c13a6
Mark Gardner <themarkgardner@gmail.com>
2023-02-24 15:50:15 -0600

    changed to react env

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+// dotenv.config()
 
 
 console.log(process.env) 
