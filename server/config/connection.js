c56a571c2da6ac3ea8cb9931777607b8932f96e6
Mark Gardner <themarkgardner@gmail.com>
2023-02-27 14:38:37 -0600

    fixed error in uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ console.log(`REACT_APP_MONGODB_URI: ${process.env.REACT_APP_MONGODB_URI}`);
 
 mongoose.connect(
   // process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-luxecavallo',
-  process.env.REACT_APP_MONGODB_URI,
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
